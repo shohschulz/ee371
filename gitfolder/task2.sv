158da2d476e4b156331ae7a4c8447372a870f29a
across04 <129254658+across04@users.noreply.github.com>
2023-11-02 14:21:53 -0700

    Update task2.sv

--- gitfolder/task2.sv
+++ gitfolder/task2.sv
@@ -5,60 +5,73 @@ module task2(SW7, SW6, SW5, SW4, SW3, SW2, SW1, SW0, HEX1, HEX0, KEY3, KEY0, LED
 	output logic [6:0] HEX1, HEX0;
 	
 	output logic LEDR9, LEDR0;
-
 	
-	logic [7:0] A;
-	logic search_up, search_low, less_than, greater_than;
+	assign start = ~KEY[3];
+	assign reset = ~KEY[0];
+	
+	logic [7:0] A, middle;
+	logic [5:0] upper, lower;
+	logic search_up, search_low, loadReady, finished, Found;
 	
 	
 	assign A = {SW7, SW6, SW5, SW4, SW3, SW2, SW1, SW0};
 	
 	
 	
-	task2_control control(A, start, reset, less_than, greater_than, search_up, search_low, search_mid, clk);
+	task2_control control(A, start, middle, upper, lower, reset, search_up, search_low, loadReady, finished, Found, clk);
 	
-	task2_dp datapath(A, search_up, search_low, search_mid, Loc, .Done(LEDR9), .Found(LEDR0), less_than, greater_than, clk);
+	task2_dp datapath(A, search_up, search_low, loadReady, finished, Loc, Done, Found, upper, middle, lower, clk);
 
 endmodule 
 
 
-module task2_control(A, start, reset, less_than, greater_than, search_up, search_low, search_mid, clk);
+module task2_control(A, start, middle, upper, lower, reset, search_up, search_low, loadReady, finished, Found, clk);
 
-	input logic start, reset, clk, less_than, greater_than;
+	input logic start, reset, clk;
+	input logic [7:0] middle; 
+	input logic [4:0] upper, lower;
 	input logic [7:0] A;
-	output logic search_up, search_low, search_mid;
+	output logic search_up, search_low, loadReady, finished, Found;
 
-	enum{S0, S1, S1, S3} ps, ns;
+	enum{S0, S1, S2, S3} ps, ns;
 	always_comb begin
+	search_up = 0; search_low = 0; loadReady = 0; finished = 0; Found = 0;
 		case(ps)
 			S0: begin //idle state
 				if(start) ns = S1;
-				else ns = ps;
+				
+				else loadReady = 1; ns = ps;
 			end
 			S1: begin //starting state, pick center value of array in datapath
-				 search_mid = 1;
-				 if(less_than) ns = S2;
-				 if(greater_than) ns = S3;
-				 else ns = ps;
+				if (upper == lower) ns = S3;
+				else if(A < middle) begin 
+					search_low = 1; 
+					ns = ps; 
+				end
+				else if(A > middle) begin 
+					search_up = 1; 
+					ns = ps;
+				end
+				else ns = S2;
 				 
 			end
-			S2: begin //less_than state, if our value is less than the found value, we 
-						 //grab center value between the lower bound and found address
-				search_low = 1;
-				
+			S2: begin //done state
+				finished = 1;
+				Found = 1;
+				ns = ps;
 			
 			end
-			S3: begin //greater_than state, if our value is greater than the found value, we 
-						 //grab center value between the upper bound and found address
-				search_up = 1;
-			
+			S3: begin 
+				finished = 1;
+				Found = 0;
+				ns = ps;
+				
 			end
 			
 			default: ns = S0;
-		end
-
+		endcase
+	end
 endmodule 
-
 
 
 
@@ -108,6 +121,7 @@ module task2_dp(A, search_up, search_low,loadReady, finished, Loc, Done, Found, 
 	assign middleAddr = (upper - lower) / 2;
 		
 endmodule 
-	
+
+
 
 
