0a201dd2c0bbfd1e8873db18506b94f24677d564
shohschulz <128335228+shohschulz@users.noreply.github.com>
2023-11-02 10:03:07 -0700

    Add files via upload

--- gitfolder/BCA.sv
+++ gitfolder/BCA.sv
@@ -1,19 +1,19 @@
-module BCA_cntrl(clk, start, A, reset, zeroFlag, A0, Load_A, result, rShift, done, increment, loadReady);
+module BCA_cntrl(clk, start, reset, zeroFlag, A0, result, rShift, done, increment, loadReady);
 	input logic start, reset, clk; 
-	input logic [7:0] A; 
+	 
 	
 	//status signals
 	input logic zeroFlag, A0; 
 	
 	//control signals
 	output logic rShift, result, done, increment, loadReady; 
-	output logic [7:0] Load_A;
+	
 	
 	
 	enum {S1, S2, S3} ps, ns; 
 	
 always_comb begin
-    result = 0; done = 0; loadReady = 0; increment = 0; rShift = 0; Load_A = 8'b0;
+    result = 0; done = 0; loadReady = 0; increment = 0; rShift = 0;
     
     case (ps)
         S1: begin
@@ -24,7 +24,6 @@ always_comb begin
             end
             else begin
                 // Load A
-                Load_A = A;
                 loadReady = 1;
 					 ns = S1; 
             end
@@ -63,9 +62,47 @@ end
 	end
 endmodule
 
-module BCA_datapath(clk, Load_A, rShift, result, done, increment, loadReady, zeroFlag, A0, sumToBoard, finished);
+module BCAcntrltb();
+	logic start, reset, clk; 
+	logic [7:0] A; 
+	
+	//status signals
+	logic zeroFlag, A0; 
+	
+	//control signals
+	logic rShift, result, done, increment, loadReady; 
+	logic [7:0] Load_A;
+	
+	BCA_cntrl dut (.*);
+	
+	parameter CLOCK_PERIOD = 100;
+	initial begin 
+		clk <= 0; 
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	
+	end
+	initial begin
+	//set A
+		reset <= 1; A0 <= 0; zeroFlag <= 0; start <= 0; @(posedge clk);
+		reset <= 0; @(posedge clk);
+		start <= 0; @(posedge clk); //Load_ready, stay in s1
+		start <= 1;  @(posedge clk); //go to S2, 
+		A0 <= 1; @(posedge clk); //increment, rShift, stay in S2
+		A0 <= 1;	@(posedge clk); //increment, again stay in S2;
+		A0 <= 0; @(posedge clk); //no increment, stay in S2
+					@(posedge clk);
+		zeroFlag <= 1; @(posedge clk);
+							@(posedge clk);			//should be in S3 and stay
+							@(posedge clk);
+		$stop;
+	end
+	
+endmodule
+
+
+module BCA_datapath(clk, A, rShift, result, done, increment, loadReady, zeroFlag, A0, sumToBoard, finished);
 	input logic clk, rShift, result, done, increment;
-	input logic [7:0] Load_A;
+	input logic [7:0] A;
 	input logic loadReady;
 	
 	//status signals
@@ -73,13 +110,13 @@ module BCA_datapath(clk, Load_A, rShift, result, done, increment, loadReady, zer
 	
 	//wires for output to board operations
 	logic [3:0] sum;
-	logic [7:0] A; 
+	logic [7:0] Awire; 
 	
 	//outputs to the board
 	output logic [3:0] sumToBoard;
 	output logic finished; 
 	
-	//if finished sum goes back to zero
+	
 	assign sumToBoard = done ? sum : '0; 
 	
 	
@@ -87,22 +124,58 @@ module BCA_datapath(clk, Load_A, rShift, result, done, increment, loadReady, zer
 	
 	//control signals from cntrl determines each RTL operation. 
 	always_ff @(posedge clk) begin
-			//from control
-			if(loadReady)
-				A <= Load_A;
+			//needs a reset state 
+			//should work as intialization 
+			zeroFlag = 0; 
+			//acts as our reset in our datapath
+			if(result) begin
+				Awire <= 0;
+				sum <= 0; 
+			end
+			else if(loadReady)
+				Awire <= A;
 			else if(rShift) 
-				A <= A >> 1;
-			else if(result)
-				A <= 0;
+				Awire <= A >> 1;
+			
 			else if(increment)
 				sum <= sum + 1;
 			
 			//status signals
 			
-			if(A == 0)  
+			if(Awire == 0)  
 				zeroFlag = 1; 
 			else 
-				A0 = A[0];
+				A0 = Awire[0];
 	end
 	 
+endmodule
+
+
+module BCA_datapathtb();
+	logic clk, rShift, result, done, increment;
+	logic [7:0] A;
+	logic loadReady;
+	logic zeroFlag, A0; 
+	logic [3:0] sumToBoard;
+	logic finished; 
+	parameter CLOCK_PERIOD = 100;
+	initial begin 
+		clk <= 0; 
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	
+	end
+	
+	BCA_datapath dut (.*);
+	
+	initial begin
+	
+	rShift <= 0; result <= 0; done <= 0; increment <= 0; A <= 8'b00000000; loadReady <= 0; result <= 1; @(posedge clk);
+	result <= 0; @(posedge clk);
+	loadReady <= 1; A <= 8'b00001111; @(posedge clk); //Awire should be equal to A
+	rShift <= 1; loadReady <= 0; @(posedge clk); //should RShift
+	result <= 1; @(posedge clk); //Awire = 0, zero flag
+	increment <= 1; result <= 0; @(posedge clk); // should increment
+	@(posedge clk);
+	$stop;
+	end
 endmodule
