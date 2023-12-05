1821d7c89641809f074c09da1956e6147e1afc52
shohschulz <128335228+shohschulz@users.noreply.github.com>
2023-12-04 16:43:43 -0800

    Update ObstacleVideoFormat.sv
    syntax

--- FlappyBird SV/ObstacleVideoFormat.sv
+++ FlappyBird SV/ObstacleVideoFormat.sv
@@ -8,7 +8,7 @@ module ObstacleVideoFormat (clk, reset, x, yBot, yTop);
     input logic [8:0] yBot, yTop;
     output logic [9:0] finalX;
     output logic [8:0] finalYBot, finalYTop;
-    output logic [9:0] yScreenMin, yScreenMaz;
+    output logic [9:0] yScreenMin, yScreenMax;
 
     assign finalX = x; 
     assign finalYBot = Constants::SCREEN_HEIGHT - (yBot);
