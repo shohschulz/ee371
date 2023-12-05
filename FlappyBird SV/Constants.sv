ad3eb1653b57fdb5646753d706921f7f851b543e
shohschulz <128335228+shohschulz@users.noreply.github.com>
2023-12-04 16:40:24 -0800

    Add files via upload

---
+++ FlappyBird SV/Constants.sv
@@ -0,0 +1,16 @@
+package Constants;
+    
+    //Screen size
+    parameter SCREEN_HEIGHT = 9'd480;
+    parameter SCREEN_WIDTH = 10'd640;
+
+    //Obstacle sizes
+    parameter OBSTACLE_MIN_Y = 6'd50;
+    parameter OBSTACLE_MAX_Y = 8'd200;
+    parameter OBSTACLE_MAX_DISTANCE = 10'd680;
+    parameter OBSTACLE_WIDTH = 6'd40;
+    parameter TIME_TO_OBSTACLE = 6'd50; //tbd
+
+    //Bird Width
+    
+    parameter BIRD_WIDTH = 6'40; 
