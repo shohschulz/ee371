8734acb6be0b4313553111cc640a3b6a39c3a98a
shohschulz <128335228+shohschulz@users.noreply.github.com>
2023-12-06 15:15:22 -0800

    Update gameOverStorage.sv

--- FlappyBird SV/gameOverStorage.sv
+++ FlappyBird SV/gameOverStorage.sv
@@ -1,28 +1,36 @@
-
- Download
-module gameOverStorage (address, clock, data, wren, largest_value);
-	logic [9:0] memory[31:0]; 
-	input logic [4:0] address;
-	input logic [9:0] data; 
-	input logic wren; 
-	input logic clock;
-    output [9:0] largest_value;
-	
-	always_ff @(posedge clock) begin
-		if(wren == 1) begin 	
-			memory[address] = data; 
-		end
-		// do nothing
-	end
-    //return the highest number 
-    // Sequential search for the largest value
-    always_comb begin
-        largest_value = memory[0];
-        for (int i = 1; i < 32; i++) begin
-            if (memory[i] > largest_value) begin
-                largest_value = memory[i];
-            end
+//Every time a game has concluded the score from that game will be written to memory
+ //Outputs 3 highest scores from the memory;
+module gameOverStorage (address, clock, data, wren, largest_values);
+    logic [9:0] memory[31:0]; 
+    input logic [4:0] address;
+    input logic [9:0] data; 
+    input logic wren; 
+    input logic clock;
+    output logic [9:0] largest_values[2:0]; // Store the three largest values
+    
+    always_ff @(posedge clock) begin
+        if(wren == 1) begin     
+            memory[address] = data; 
         end
     end
 
+    // Return the three highest numbers 
+    // Sequential search for the three largest values
+    always_comb begin
+        largest_values = {10'b0, 10'b0, 10'b0}; // Initialize the three largest values
+        for (int i = 0; i < 32; i++) begin
+            if (memory[i] > largest_values[0]) begin
+                largest_values[2] = largest_values[1];
+                largest_values[1] = largest_values[0];
+                largest_values[0] = memory[i];
+            end
+            else if (memory[i] > largest_values[1]) begin
+                largest_values[2] = largest_values[1];
+                largest_values[1] = memory[i];
+            end
+            else if (memory[i] > largest_values[2]) begin
+                largest_values[2] = memory[i];
+            end
+        end
+    end
 endmodule
